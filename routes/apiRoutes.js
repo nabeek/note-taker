8ad567687f9045c35ca51d9f387b7827c7a01ced
nabeek <nabeek@gmail.com>
2020-05-07 11:02:06 -0600

    Server functioning and notes storing to db

---
+++ routes/apiRoutes.js
@@ -0,0 +1,12 @@
+const notesData = require("../db/db.json");
+
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(notesData);
+  });
+
+  app.post("/api/notes", function (req, res) {
+    notesData.push(req.body);
+    res.json(true);
+  });
+};
